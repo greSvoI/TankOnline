bc072d9fcae465cda2111dcbd57432618d64cda2
Сергей <serega.iva@outlook.com>
2021-02-21 09:10:23 +0300

    Добавьте файлы проекта.

---
+++ Serialization/Serialization.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    public class Serialization
+    {
+		public static byte[] ObjectToByteArray(Object obj)
+		{
+			if (obj == null)
+				return null;
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			//Убирате привязку к сборке Serialization
+			binaryFormatter.AssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple;
+			MemoryStream memoryStream = new MemoryStream();
+			binaryFormatter.Serialize(memoryStream, obj);
+
+			return memoryStream.ToArray();
+		}
+		public static Object ByteArrayToObject(byte[] arrayByte)
+		{
+			MemoryStream memoryStream = new MemoryStream();
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			binaryFormatter.Binder = new Binder();
+			//Убирате привязку к сборке Serialization
+			binaryFormatter.AssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple;
+			memoryStream.Write(arrayByte, 0, arrayByte.Length);
+			memoryStream.Seek(0, SeekOrigin.Begin);
+			Object obj = (Object)binaryFormatter.Deserialize(memoryStream);
+
+			return obj;
+		}
+	}
+	public class Binder : SerializationBinder
+	{
+		public override Type BindToType(string assemblyName, string typeName)
+		{
+			Assembly currentasm = Assembly.GetExecutingAssembly();
+
+			return Type.GetType($"{currentasm.GetName().Name}.{typeName.Split('.')[1]}");
+		}
+	}
+	[Serializable]
+	public class ObjectAction
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Speed { get; set; }
+		public bool Up { get; set; } = true;
+		public bool Down { get; set; } = false;
+		public bool Left { get; set; } = false;
+		public bool Righ { get; set; } = false;
+		public bool Shot { get; set; } = false;
+	}
+}
---
+++ TankOnline/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace TankOnline
+{
+	public partial class Form1 : Form
+	{
+		TcpListener tcpListener;
+		TcpClient tcpClient;
+		NetworkStream networkStream;
+		IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async void buttonWait_Click(object sender, EventArgs e)
+		{
+			foreach (Control control in Controls)
+				control.Visible = false;
+			try
+			{
+				if(textBox1.Text == "")
+				{
+					tcpListener = new TcpListener(iPAddress, 8000);
+					tcpListener.Start();
+					await Task.Run(new Action(() => tcpClient = tcpListener.AcceptTcpClient()));
+					networkStream = tcpClient.GetStream();
+				}
+				else
+				{
+					tcpListener = new TcpListener(IPAddress.Parse(textBox1.Text), 8000);
+					tcpListener.Start();
+					await Task.Run(new Action(() => tcpClient = tcpListener.AcceptTcpClient()));
+					networkStream = tcpClient.GetStream();
+				}
+
+			}
+			catch (Exception ex)
+			{
+
+				MessageBox.Show(ex.Message+" buttonWait");
+			}
+		}
+
+		private void buttonConnect_Click(object sender, EventArgs e)
+		{
+			foreach (Control control in Controls)
+				control.Visible = false;
+			try
+			{
+
+				if(textBox1.Text == "")
+				{
+					tcpClient = new TcpClient();
+					tcpClient.Connect(iPAddress, 8000);
+					networkStream = tcpClient.GetStream();
+				}
+				
+
+
+
+
+			}
+			catch (Exception ex)
+			{
+
+				MessageBox.Show(ex.Message+ " buttonConnect");
+			}
+		}
+		private void GetAction()
+		{
+
+		}
+	}
+}
